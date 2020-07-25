5f1bb3b60790068063b41de637329cfb43e6f5a4
Mateus Alencar <mtauws@gmail.com>
2020-07-24 21:51:38 -0300

    structure of the application and the css file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <>
-    <h1>Hello World</h1>
-    </>
-  );
+class AutoScalingText extends Component {
+
 }
 
-export default App;
+class CalculatorKay extends Component {
+  
+}
