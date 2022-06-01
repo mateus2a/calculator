97847e06b27fa8f9911ca01056e5362d7cbd7384
Mateus <mtauws@gmail.com>
2022-06-01 15:56:08 -0300

    style: add display flex to the div and align to center of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-unresolved */
 import * as React from 'react';
 import 'https://cdn.skypack.dev/ios-calculator-for-web';
+import './App.scss';
 
 function App(): JSX.Element {
   return (
-    <div>
+    <div className="App">
       <ios-calculator />
     </div>
   );
